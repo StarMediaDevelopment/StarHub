37cc2544516a4a3f7a0582075577afa3e0cb8c9c
AloneDevelopment <Alonedevelopments@gmail.com>
2021-07-03 19:13:19 +0800

    Added to JoinEvent.java

--- src/main/java/com/starmediadev/plugins/starhub/events/JoinEvent.java
+++ src/main/java/com/starmediadev/plugins/starhub/events/JoinEvent.java
@@ -1,13 +1,50 @@
 package com.starmediadev.plugins.starhub.events;
 
+import com.starmediadev.plugins.starhub.StarHub;
+import com.starmediadev.plugins.starhub.functions.MessageFunctions;
+import com.starmediadev.plugins.starhub.util.Util;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinEvent implements Listener {
 
+    private MessageFunctions msg;
+    private StarHub main;
+
+    public JoinEvent(StarHub main) {
+        this.main = main;
+        this.msg = new MessageFunctions(main);
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (player.hasPermission("FireCraft.isStaff")) {
+            event.setJoinMessage(main.getConfig().getString("Join.Messages.StaffJoinMessage"));
+            msg.sendMessages(player, "Join.Messages.StaffMOTD");
+            return;
+        }
+        event.setJoinMessage(main.getConfig().getString("Join.Messages.PlayerJoinMessage"));
+        msg.sendMessages(player, "Join.Messages.MOTD");
+
+        player.setGameMode(GameMode.valueOf(main.getConfig().getString("Join.GameMode")));
+        if (main.getConfig().getBoolean("Join.ClearInventory")) {
+            player.getInventory().clear();
+        }
+        if (main.getConfig().getBoolean("Join.TeleportToSpawn")) {
+            Location spawn = new Location(Bukkit.getWorld(main.getConfig().getString("VoidTeleport.Spawn.WORLD")),
+                    main.getConfig().getDouble("VoidTeleport.Spawn.X"), main.getConfig().getDouble("VoidTeleport.Spawn.Y"),
+                    main.getConfig().getDouble("VoidTeleport.Spawn.Z"), main.getConfig().getInt("VoidTeleport.Spawn.YAW"),
+                    main.getConfig().getInt("VoidTeleport.Spawn.PITCH"));
+            player.teleport(spawn);
+        }
+
 
     }
 
